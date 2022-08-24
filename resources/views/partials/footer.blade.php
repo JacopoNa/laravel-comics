f4b61f704b6fd10152505a929eaae0f48a7ee049
JacopoNa <j.nardelli@virgilio.it>
2022-08-25 01:29:27 +0200

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,87 @@
 <footer>
-    <h1>prova footer</h1>
+    <footer>
+        <div class="footer-top">
+            <div class="container">
+                <div class="footer-list">
+                    <div>
+                        <ul>
+                            <li class="first-li">DC COMICS</li>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+
+                         <ul>
+                            <li class="first-li">SHOP</li>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+
+                    <ul>
+                        <li class="first-li">DC</li>
+                        <li>Terms of Use</li>
+                        <li>Privacy policy(New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertisings</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+
+                    <ul>
+                        <li class="first-li">SITES</li>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+
+                    
+                </div>
+                <div class="footer-logo">
+                    <img src="{{ asset('img/dc-logo-bg.png') }}" alt="logo">
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="sign">
+                    <div>SIGN-UP NOW!</div>
+                </div>
+
+                <div class="socials">
+                    <ul>
+                        <li class="follow">FOLLOW US</li>
+                        <li>
+                            <img src="{{ asset('img/footer-facebook.png') }}" alt="socials">
+                        </li>
+                        <li>
+                            <img src="{{ asset('img/footer-twitter.png') }}" alt="socials">
+                        </li>
+                        <li>
+                            <img src="{{ asset('img/footer-youtube.png') }}" alt="socials">
+                        </li>
+                        <li>
+                            <img src="{{ asset('img/footer-periscope.png') }}" alt="socials">
+                        </li>
+                        <li>
+                            <img src="{{ asset('img/footer-pinterest.png') }}" alt="socials">
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
 </footer>
